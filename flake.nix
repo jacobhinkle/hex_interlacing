65dd4fe471478d8fe09df7edaf3833e99c0e9195
Jacob Hinkle <jacob.hinkle@jhink.org>
2022-08-25 08:32:32 -0400

    Fixed notebook (color = ... else 1), and fixed up flake

--- flake.nix
+++ flake.nix
@@ -5,22 +5,35 @@
     flake-utils.url = "github:numtide/flake-utils/v1.0.0";
   };
 
-  outputs = { self, nixpkgs, flake-utils }: 
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system}; in
-      rec {
-        packages = flake-utils.lib.flattenTree {
-          #hexpdf = {};
-          #hello = pkgs.hello;
-          #gitAndTools = pkgs.gitAndTools;
+  outputs = { self, nixpkgs, flake-utils }: {
+      packages.x86_64-linux.default =
+        with import nixpkgs { system = "x86_64-linux"; };
+        stdenv.mkDerivation {
+          name = "hexpdf";
+          src = self;
+          buildInputs = with pkgs; [
+            texlive.combined.scheme-full
+          ] ++ (with pkgs.python3Packages; [
+            jupyter
+            matplotlib
+            numpy
+            ipympl
+          ]);
+          buildPhase = ''
+            jupyter nbconvert --inplace --execute notebooks/HexInterlacing.ipynb
+            cd latex
+            latexmk -pdf hex.tex
+            cd ..
+            '';
+          installPhase = "mkdir -p $out && cp latex/hex.pdf $out/";
         };
-        defaultPackage = packages.hexpdf;
-      }
-    );
-    flake-utils.lib.simpleFlake {
-      inherit self nixpkgs;
-      name = "Hexagonal scan interlacing paper";
-      shell = ./shell.nix;
-      systems = [ "x86_64-linux" ];
-    };
+      #devShells = { default=import ./shell.nix {inherit pkgs;}; };
+    #);
+    #flake-utils.lib.simpleFlake {
+      #inherit self nixpkgs;
+      #name = "Hexagonal scan interlacing paper";
+      #shell = ./shell.nix;
+      #systems = [ "x86_64-linux" ];
+    #};
+  };
 }
