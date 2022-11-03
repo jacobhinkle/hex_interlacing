267d76d0954e3a0fb311a124eb711ef1fd8d8d2b
Jacob Hinkle <jacob@jhink.org>
2022-11-03 11:43:25 -0400

    Remove figs from figs/. Add back missing montage figure code

---
+++ figs/README.md
@@ -0,0 +1,2 @@
+**NOTE**: this directory will be filled automatically when running the included
+Jupyter notebook.
--- flake.nix
+++ flake.nix
@@ -25,21 +25,22 @@
     in
       rec {
         packages = flake-utils.lib.flattenTree rec {
-          hex-paper-figures = pkgs.stdenv.mkDerivation {
-            name = "hex-paper-figures";
+          figures = pkgs.stdenv.mkDerivation {
+            name = "figures";
             meta.description = "Generate all figures from jupyter notebook.";
             src = ./.;
             buildInputs = jupyterEnvInputs;
             buildPhase = ''
+              rm -f figs/*.pdf
               jupyter nbconvert --inplace --execute notebooks/HexInterlacing.ipynb
               '';
             installPhase = "mkdir -p $out && cp figs/*.pdf $out/";
           };
-          hex-paper-latex = pkgs.stdenv.mkDerivation {
-            name = "hex-paper-latex";
+          latex = pkgs.stdenv.mkDerivation {
+            name = "latex";
             meta.description = "Collect generated figures and latex file.";
             src = ./latex;
-            buildInputs = [ hex-paper-figures ];
+            buildInputs = [ figures ];
             # buildPhase creates a flat directory structure, adjusting paths in
             # hex.tex to point to current directory
             buildPhase = ''
@@ -48,24 +49,24 @@
             installPhase = ''
               mkdir -p $out
               cp ./hex.tex ./hex.bib $out
-              for i in ${hex-paper-figures}/*
+              for i in ${figures}/*
               do
                 ln -s $i $out/
               done
               '';
           };
-          hex-paper-latex-zip = pkgs.stdenv.mkDerivation {
-            name = "hex-paper-latex.zip";
+          latex-zip = pkgs.stdenv.mkDerivation {
+            name = "latex.zip";
             meta.description = "Zip up latex source in format suitable for submission.";
             src = ./latex;
-            buildInputs = [ hex-paper-latex pkgs.zip ] ++ latexDeps;
+            buildInputs = [ latex pkgs.zip ] ++ latexDeps;
             buildPhase = ''
               # build in build dir, just to generate a .bbl file, then copy to
               # zip with original source
               mkdir build zip
               (
                 cd build
-                for i in ${hex-paper-latex}/*
+                for i in ${latex}/*
                 do
                   ln -s $i .
                 done
@@ -74,7 +75,7 @@
               )
               (
                 cd zip
-                for i in ${hex-paper-latex}/*
+                for i in ${latex}/*
                 do
                   ln -s $i .
                 done
@@ -85,17 +86,17 @@
               cp hex-paper-latex.zip $out
               '';
           };
-          hexpdf = pkgs.stdenv.mkDerivation {
+          pdf = pkgs.stdenv.mkDerivation {
             name = "hex.pdf";
             meta.description = "Compile latex with generated figures.";
             src = self;
-            buildInputs = [ hex-paper-latex ] ++ latexDeps;
+            buildInputs = [ latex ] ++ latexDeps;
             buildPhase = ''
               # create writeable latex dir. Link in all source files
               mkdir build
               (  # build in subshell
                 cd build
-                for i in ${hex-paper-latex}/*
+                for i in ${latex}/*
                 do
                   ln -s $i .
                 done
@@ -105,9 +106,7 @@
             installPhase = "cp build/hex.pdf $out";
           };
         };
-        defaultPackage = packages.hexpdf;
-        #apps.jupyter = jupyter
-        #defaultApp = jupyter;
+        defaultPackage = packages.pdf;
       }
     );
 }
